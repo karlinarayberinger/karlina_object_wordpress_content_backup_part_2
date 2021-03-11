e417405a3509f1c33f8e9eb7721f9ee7355ed0b3
Karlina Ray Beringer <karlina.ray.beringer@protonmail.com>
2021-03-11 12:22:24 -0800

    Add files via upload

---
+++ binary_to_decimal.js
@@ -0,0 +1,29 @@
+/**
+ * file: binary_to_decimal.js
+ * type: JavaScript
+ * author: karbytes
+ * date: 11_MARCH_2021
+ * license: PUBLIC_DOMAIN
+ */
+
+function generate_random_nonnegative_number_less_than_one() {
+	try {
+		let random_real_number_in_base_ten = Math.random();
+		return random_real_number_in_base_ten;
+	}
+	catch(error) {
+		console.log("An error during the runtime of generate_random_nonnegative_number_less_than_one(): " + error);
+ 		return 0;
+	}
+}
+
+function generate_random_nonnegative_integer_less_than_one_hundred() {
+ 	try {
+
+ 	}
+ 	catch(error) {
+ 		console.log("An error during the runtime of generate_random_nonnegative_integer_less_than_one_hundred(): " + error);
+ 		return 0;
+ 	}
+}
+

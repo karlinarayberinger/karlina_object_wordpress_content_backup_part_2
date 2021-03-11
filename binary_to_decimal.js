b89a0f6eb994cfa17d19771c605e36ce97918eb1
Karlina Ray Beringer <karlina.ray.beringer@protonmail.com>
2021-03-11 12:29:46 -0800

    Add files via upload

--- binary_to_decimal.js
+++ binary_to_decimal.js
@@ -8,7 +8,8 @@
 
 function generate_random_nonnegative_number_less_than_one() {
 	try {
-		let random_real_number_in_base_ten = Math.random();
+		let random_real_number_in_base_ten = 0;
+		random_real_number_in_base_ten = Math.random(); // Use native JavaScript randomness function from the JavaScript Math library.
 		return random_real_number_in_base_ten;
 	}
 	catch(error) {
@@ -19,7 +20,11 @@ function generate_random_nonnegative_number_less_than_one() {
 
 function generate_random_nonnegative_integer_less_than_one_hundred() {
  	try {
-
+ 		let random_real_number_in_base_ten = 0;
+ 		random_real_number_in_base_ten = generate_random_nonnegative_number_less_than_one();
+ 		random_real_number_in_base_ten = (random_real_number_in_base_ten * 100);
+ 		random_real_number_in_base_ten = Math.floor(random_real_number_in_base_ten); // Round down to nearest integer.
+ 		return random_real_number_in_base_ten;
  	}
  	catch(error) {
  		console.log("An error during the runtime of generate_random_nonnegative_integer_less_than_one_hundred(): " + error);

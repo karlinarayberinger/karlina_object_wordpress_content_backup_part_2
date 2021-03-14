eeb58dd98995c6556685829b974513e83ccdeba9
Karlina Ray Beringer <karlina.ray.beringer@protonmail.com>
2021-03-14 01:17:22 -0800

    Add files via upload

--- binary_to_decimal.js
+++ binary_to_decimal.js
@@ -2,134 +2,48 @@
  * file: binary_to_decimal.js
  * type: JavaScript
  * author: karbytes
- * date: 11_MARCH_2021
+ * date: 14_MARCH_2021
  * license: PUBLIC_DOMAIN
  */
 
-function generate_random_nonnegative_number_less_than_one() {
-	try {
-		let random_real_number_in_base_ten = 0;
-		random_real_number_in_base_ten = Math.random(); // Use native JavaScript randomness function from the JavaScript Math library.
-		return random_real_number_in_base_ten;
-	}
-	catch(error) {
-		console.log("An error during the runtime of generate_random_nonnegative_number_less_than_one(): " + error);
- 		return 0;
-	}
+function random_nonnegative_real_number() {
+	return Math.random();
 }
 
-function generate_random_nonnegative_integer_less_than_one_hundred() {
- 	try {
- 		let random_real_number_in_base_ten = 0;
- 		random_real_number_in_base_ten = generate_random_nonnegative_number_less_than_one();
- 		random_real_number_in_base_ten = (random_real_number_in_base_ten * 100);
- 		random_real_number_in_base_ten = Math.floor(random_real_number_in_base_ten); // Round down to nearest integer.
- 		return random_real_number_in_base_ten;
- 	}
- 	catch(error) {
- 		console.log("An error during the runtime of generate_random_nonnegative_integer_less_than_one_hundred(): " + error);
- 		return 0;
- 	}
+function random_nonnegative_integer() {
+	return Math.floor(random_nonnegative_real_number() * 100);
 }
 
-function generate_random_binary_digit() {
-	try {
-		let random_nonnegative_integer_in_base_ten = 0;
-		let random_binary_digit = 0;
-		random_nonnegative_integer_in_base_ten =  generate_random_nonnegative_integer_less_than_one_hundred();
-		random_binary_digit = (random_nonnegative_integer_in_base_ten % 2); // Get the remainder of random_nonnegative_integer_in_base_ten divided by 2.
-		return random_binary_digit;
-	}
-	catch(error) {
-		console.log("An error during the runtime of generate_random_binary_digit(): " + error);
- 		return 0;
-	}
+function random_binary_digit() {
+	return ((random_nonnegative_integer() % 2) === 0) ? 0 : 1;
 }
 
-function append_binary_digit_to_left_end_of_binary_digit_sequence_no_longer_than_eight_digits(binary_digit) {
-	try {
-		let binary_digit_string = '';
-		let binary_digit_sequence_string = "";
-		let binary_digit_sequence_HTML_span_element = {};
-		if (arguments.length !== 1) {
-			throw "Exactly one function argument is required.";
-		}
-		if (typeof arguments[0] !== "number") {
-			throw "The function argument must be a JavaScript Number type datum.";
-		}
-		if ((binary_digit !== 0) || (binary_digit !== 1)) {
-			throw "The function argument must be either the JavaScript Number type datum 0 or else the JavaScript Number type datum 1.";
-		}
-		if (typeof document.getElementById("binary_digit_sequence_HTML_span_element").innerHTML !== "string") {
-			throw 'The corresponding web page does not appear to have a span element whose identifier is "binary_digit_sequence_HTML_span_element".';
-		}
-		binary_digit_sequence_HTML_span_element = document.getElementById("binary_digit_sequence_HTML_span_element");
-		binary_digit_sequence_string = binary_digit_sequence_HTML_span_element.innerHTML;
-		if ((binary_digit_sequence_string === 7) || (binary_digit_sequence_string < 7)) {
-			throw "The binary_digit_sequence_string must not exceed a length of eight characters.";
-		}
-		binary_digit_string = ('' + binary_digit);
-		binary_digit_sequence_string = (binary_digit_sequence_string + binary_digit_string);
-		binary_digit_sequence_HTML_span_element.innerHTML = binary_digit_sequence_string;
-	}
-	catch(error) {
-		console.log("An error during the runtime of append_binary_digit_to_left_end_of_binary_digit_sequence_no_longer_than_eight_digits(binary_digit): " + error);
-	}
+function random_binary_term() {
+	let binary_term_array = [], array_index = 0;
+	for (array_index = 0; array_index < 7; array_index +=1) binary_term_array.push(random_binary_digit());
+	return binary_term_array;
 }
 
-function append_binary_digit_zero_to_left_end_of_binary_digit_sequence_no_longer_than_eight_digits() {
-	try {
-		append_binary_digit_to_left_end_of_binary_digit_sequence_no_longer_than_eight_digits(0);
-	}
-	catch(error) {
-		console.log("An error during the runtime of append_binary_digit_zero_to_left_end_of_binary_digit_sequence_no_longer_than_eight_digits(): " + error);
-	}
+function validate_binary_term_array(input) {
+	
 }
 
-function append_binary_digit_one_to_left_end_of_binary_digit_sequence_no_longer_than_eight_digits() {
-	try {
-		append_binary_digit_to_left_end_of_binary_digit_sequence_no_longer_than_eight_digits(1);
-	}
-	catch(error) {
-		console.log("An error during the runtime of append_binary_digit_one_to_left_end_of_binary_digit_sequence_no_longer_than_eight_digits(): " + error);
-	}
+function extract_binary_term_array_from_interface() {
+
 }
 
-function append_random_binary_digit_to_left_end_of_binary_digit_sequence_no_longer_than_eight_digits() {
-	try {
-		let random_binary_digit = 0;
-		random_binary_digit = generate_random_binary_digit();
-		append_binary_digit_to_left_end_of_binary_digit_sequence_no_longer_than_eight_digits(random_binary_digit);
-	}
-	catch(error) {
-		console.log("An error during the runtime of append_random_binary_digit_to_left_end_of_binary_digit_sequence_no_longer_than_eight_digits(): " + error);
-	}
+function update_binary_input_display() {
+
 }
 
-function A() {
-	try {
-		append_random_binary_digit_to_left_end_of_binary_digit_sequence_no_longer_than_eight_digits(0);
-	}
-	catch(error) {
-		console.log("An error during the runtime of A(): " + error);
-	}
+function update_decimal_output_display() {
+
 }
 
-function B() {
-	try {
-		append_random_binary_digit_to_left_end_of_binary_digit_sequence_no_longer_than_eight_digits(1);
-	}
-	catch(error) {
-		console.log("An error during the runtime of B(): " + error);
-	}
+function update_base_conversion_calculations_display() {
+
 }
 
-function C() {
-	try {
-		append_random_binary_digit_to_left_end_of_binary_digit_sequence_no_longer_than_eight_digits();
-	}
-	catch(error) {
-		console.log("An error during the runtime of C(): " + error);
-	}
+function binary_to_decimal() {
+	let binary_term_array = [], decimal_integer_result = 0, base_conversion_calculations_string = "";
 }
-

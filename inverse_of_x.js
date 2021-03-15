d84a24f27bc7a7438dc6de484353f6b82b51221b
Karlina Ray Beringer <karlina.ray.beringer@protonmail.com>
2021-03-14 22:45:00 -0700

    Add files via upload

--- inverse_of_x.js
+++ inverse_of_x.js
@@ -18,3 +18,9 @@ function round_down_to_nearest_integer(input) {
 	if (!is_exactly_one_number_type_argument(input)) return 0;
 	return Math.floor(input);
 }
+
+function round_down_to_nearest_absolute_value_integer(input) {
+	if (!is_exactly_one_number_type_argument(input)) return 0;
+	input = (input > 0) ? input : ((-1) * input);
+	return Math.floor(input);
+}

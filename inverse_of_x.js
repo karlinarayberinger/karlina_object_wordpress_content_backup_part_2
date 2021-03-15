d3ca01d7ee042b3e1c55ffb3983dc4173a677911
Karlina Ray Beringer <karlina.ray.beringer@protonmail.com>
2021-03-14 22:40:19 -0700

    Add files via upload

---
+++ inverse_of_x.js
@@ -0,0 +1,19 @@
+/**
+ * file: inverse_of_x.js
+ * type: JavaScript
+ * author: karbytes
+ * date: 14_MARCH_2021
+ * license: PUBLIC_DOMAIN
+ */
+
+function is_exactly_one_argument(input) {
+	return (arguments.length === 1) ? true : false;
+}
+
+function is_exactly_one_number_type_argument(input) {
+	return (is_exactly_one_argument(input) && (typeof input === "number")) ? true : false;
+}
+
+function round_down_to_nearest_integer(input) {
+
+}
